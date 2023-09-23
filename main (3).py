721776bbff3e2f4903a55426b39f261837881d93
as2222j0695 <144312375+as2222j0695@users.noreply.github.com>
2023-09-23 10:11:42 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,22 +1,39 @@
-# Define the base class player 
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
+'''
+Implement a function called sort_students that takes a list of student objects as input and sorts the
+list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object
+has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function
+with different input lists of students.
+'''
 
-# Define the deived class Batsman
-class Batsman(Player):
-    def play(self):
-        print ("The batsman is batting.")
+class Student:
 
-# Define the deribed class Bowler
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
 
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+
+
+# Example usage:
+students = [
+    Student("Hari", "A123", 7.8),
+    Student("Srikanth", "A124", 8.9),
+    Student("Saumya", "A125", 9.1),
+    Student("Mahidhar", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
