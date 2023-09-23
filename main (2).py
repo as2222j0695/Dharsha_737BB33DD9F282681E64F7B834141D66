3f2925b5108908ade7a33867551e611f10f3e1f4
as2222j0695 <144312375+as2222j0695@users.noreply.github.com>
2023-09-23 10:10:54 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,50 +1,24 @@
-class BankAccount:
-    def __init__(self, account_number, date_of_opening, balance, customer_name):
-        self.account_number = account_number
-        self.date_of_opening  = date_of_opening 
-        self.balance = balance
-        self.customer_name = customer_name
-        
-    def deposit(self, amount):
-        self.balance += amount
-        print(f"${amount} has been deposited in your account.")
-    
-    def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient balance.")
-        else:
-            self.balance -= amount
-            print(f"${amount} has been withdrawn from your account.")
-            
-    def check_balance(self):
-        print(f"Current balance is ${self.balance}.")
-        
-    def print_customer_details(self):
-        print("Name:", self.customer_name)
-        print("Account Number:", self.account_number)
-        print("Date of opening:", self.date_of_opening)
-        print(f"Balance: ${self.balance}\n")   
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
 
-# Input customer details
-ac_no_1 = BankAccount(2345, "01-01-2011", 1000, "Toninho Takeo")
-ac_no_2 = BankAccount(1234, "11-03-2011", 2000, "Astrid Rugile")
-ac_no_3 = BankAccount(2312, "12-01-2009", 3000, "Orli Kerenza")
-ac_no_4 = BankAccount(1395, "01-01-2011", 3000, "Luciana Chika")
-ac_no_5 = BankAccount(6345, "01-05-2011", 4000, "Toninho Takeo")
 
-print("Customer Details:")
-ac_no_1.print_customer_details()
-ac_no_2.print_customer_details()
-ac_no_3.print_customer_details()
-ac_no_4.print_customer_details()
-ac_no_5.print_customer_details()
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-print("=============================")
-ac_no_4.print_customer_details()
-# Current balance is $3000.
-# $1000 has been deposited in your account.
-ac_no_4.deposit(1000)
-ac_no_4.check_balance()
-# Your current balance $4000.
-# You want to withdraw $5000
-ac_no_4.withdraw(5000)
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
